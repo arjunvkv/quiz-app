0907cca5fe90a8c716b2a4c8cb4503736f5ff39a
Arjun <arjun.vkv.97@gmail.com>
2024-07-22 15:32:16 +0530

    route fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Summary from "./Components/Summary/Summary";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route path="/summary" component={Summary} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route path="/summary" element={<Summary />} />
+      </Routes>
     </Router>
   );
 }
